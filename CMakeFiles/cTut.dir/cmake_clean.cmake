3d4033cc4dd0cae3190af1c2b891b3881a43a840
George-MG <georgem.gichuru@gmail.com>
2021-07-01 16:13:01 +0300

    Added README.md

---
+++ CMakeFiles/cTut.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cTut.dir/tutorial1.c.o"
+  "cTut"
+  "cTut.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cTut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
