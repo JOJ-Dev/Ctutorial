5d6779ef94abeee9b3ddfadc0e596391b8a4838b
George-MG <georgem.gichuru@gmail.com>
2021-07-01 15:52:20 +0300

    Testing....??

---
+++ cmake-build-debug/CMakeFiles/cTut.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cTut.dir/main.c.o"
+  "CMakeFiles/cTut.dir/test.c.o"
+  "cTut"
+  "cTut.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cTut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
